60b77be7b7de482657dcfeead882d38149844910
lidongyan <502024330056@smail.nju.edu.cn>
2025-06-29 18:07:08 +0800

    AboutMe: modify self introduction
    Signed-off-by: lidongyan <502024330056@smail.nju.edu.cn>

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,10 +4,10 @@ function Header() {
     return (<>
         <header className="header">
             <div className="header-container">
-                <h1>Lidong's HomePage</h1>
+                <h1>Lidong's Blog</h1>
                 <nav className="header-nav">
                     <a href="/">Home</a>
-                    <a href="/post">Post</a>
+                    <a href="post/index.html">Post</a>
                 </nav>
             </div>
         </header>
--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -11,9 +11,9 @@ function SideBar() {
                     >
                         <img
                             src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-                            alt="GitHub"
+                            alt="github"
                         />
-                        <span>GitHub</span>
+                        <span>github</span>
                     </a>
                     <a
                         href="tencent://AddContact/?fromId=45&fromSubId=1&subcmd=all&uin=1799513674&website=www.oicqzone.com"
@@ -21,9 +21,29 @@ function SideBar() {
                     >
                         <img
                             src="https://img.icons8.com/color/48/000000/qq.png"
-                            alt="QQ"
+                            alt="qq"
                         />
-                        <span>QQ</span>
+                        <span>qq</span>
+                    </a>
+                    <a
+                        href="mailto:yldhome2d2@gmail.com"
+                        className="social-link"
+                    >
+                        <img
+                            src="https://cdn-icons-png.flaticon.com/512/281/281764.png"
+                            alt="my gmail"
+                        />
+                        <span>gmail</span>
+                    </a>
+                    <a
+                        href="mailto:502024330056@smail.nju.edu.cn"
+                        className="social-link"
+                    >
+                        <img
+                            src="https://cdn-icons-png.flaticon.com/512/561/561127.png"
+                            alt="my school email"
+                        />
+                        <span>school email</span>
                     </a>
                 </li>
             </ul>
