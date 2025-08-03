cf36d56d140b3a155de95745f011012933106e32
Lidong Yan <yldhome2d2@gmail.com>
2025-08-03 11:19:39 +0800

    MyScreen: fix some typos
    Signed-off-by: Lidong Yan <yldhome2d2@gmail.com>

--- src/post/myscreen/introWin.jsx
+++ src/post/myscreen/introWin.jsx
@@ -26,7 +26,7 @@ function introWin() {
             ></img>
         </div>
         <p className="indent">
-            看起来非常简单，对不对？不过你也许会问：什么是伪终端（英文简称是pty）？很显然，伪终端不是真实的设备，如果你熟悉UNIX系统，伪终端看起来更像是管道。但是，伪终端除了具有管道的功能，还可以像正常的终端一样获取窗口大小，设置各种各样的属性。在上面的图片中，当shell窗口应用被打开时，shell窗口应用就会打开一个伪终端，设置伪终端的窗口大小为shell窗口应用的窗口大小，然后在伪终端的一边运行bash程序，一边读取bash程序的输出/发送用户的输入。如果用户输入了CTRL-C到伪终端设备，伪终端设备也不会像管道一样发送CTRL-C到另一端，而是会想终端一样对另一端的程序发送SIGINT信号。
+            看起来非常简单，对不对？不过你也许会问：什么是伪终端（英文简称是pty）？伪终端类似一个UNIX管道，并不是一个真实的设备。在上图中，前端（e.g. MacOS上的iTerm，下文用iTerm代表前端）把用户输入发送到伪终端，命令行程序就可以从stdin读取；命令行程序将输出写入stdout和stderr，iTerm就可以从伪终端中读取输出显示在窗口中。不过，除了具有管道的功能，伪终端还可以像正常的终端一样获取/设置窗口大小，设置各种各样的属性以及处理特殊字符。比如当iTerm被打开时，iTerm就会打开一个伪终端，设置伪终端的窗口大小为iTerm的默认窗口大小。如果iTerm发送CTRL-C到伪终端设备，伪终端设备也不会像管道一样发送CTRL-C供命令行程序读取，而是会发送SIGINT信号给命令行程序。
         </p>
         <p className="indent">
             Whew！说了这么多，终于可以谈谈screen这样的窗口管理程序该怎么实现了，一个简单的想法是这样
@@ -39,7 +39,7 @@ function introWin() {
             ></img>
         </div>
         <p className="indent">
-            这张图片中，screen程序作为前端，读取用户输入只需要在标准输入<code>STDIN_FILENO</code>上调用<code>read</code>，渲染窗口只需要把程序输出<code>printf</code>到标准输出（之后shell窗口程序——比如MacOS上的iTerm程序，会把标准输出的字符绘制到窗口中）。screen可以根据用户需求（e.g. <code>screen -a -t 0</code>）接入（attach）到一个伪终端，也可以离开（detach）当前伪终端，接入其他伪终端，这样就实现了窗口管理和切换。当然，实现screen要考虑的事情比上面说的其实还要多很多，但这篇博客中的实现会保持尽可能简单。myscreen虽然输出没有screen漂亮，不过管理和切换窗口的思想和screen是一致的。
+            这张图片中，screen程序作为前端，读取用户输入只需要在标准输入<code>STDIN_FILENO</code>上调用<code>read</code>，渲染窗口只需要把程序输出<code>printf</code>到标准输出（之后iTerm程序，会把标准输出的字符绘制到窗口中）。screen可以根据用户需求（e.g. <code>screen -a -t 0</code>）接入（attach）到一个伪终端，也可以离开（detach）当前伪终端，接入其他伪终端，这样就实现了窗口管理和切换。当然，实现screen要考虑的事情比上面说的其实还要多很多，但这篇博客中的实现会保持尽可能简单。myscreen虽然输出没有screen漂亮，不过管理和切换窗口的思想和screen是一致的。
         </p>
     </div>)
 }
